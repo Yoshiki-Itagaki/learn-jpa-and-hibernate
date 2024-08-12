8f68d8a4756f0577fa1280efb47fb23e0eaaf8ea
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-13 00:36:12 +0900

    Setting up new Stpring Boot project for JPA and Hibernate

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/LearnJpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learn_jpa_and_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJpaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ learn-jpa-and-hibernate/src/test/java/com/in28minutes/springboot/learn_jpa_and_hibernate/LearnJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.learn_jpa_and_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
