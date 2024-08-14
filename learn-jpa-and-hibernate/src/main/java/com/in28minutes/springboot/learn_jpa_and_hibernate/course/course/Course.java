43ad5a076937f87ee62fd94b6fd074ed4e76a28e
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-15 00:12:17 +0900

    Querying data using Spring JDBC

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/course/Course.java
@@ -0,0 +1,45 @@
+package com.in28minutes.springboot.learn_jpa_and_hibernate.course.course;
+
+public class Course {
+	private long id;
+	private String name;
+	private String author;
+	
+	public Course(long id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", author=" + author + "]";
+	}
+	
+
+}
--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcRepository.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.in28minutes.springboot.learn_jpa_and_hibernate.course.course.Course;
+
 @Repository
 public class CourseJdbcRepository {
 	
@@ -13,11 +15,22 @@ public class CourseJdbcRepository {
 	private static String INSERT_QUERY = 
 			"""
 				insert into course(id, name, author)
-				values(1, 'Learn AWS', 'in28minutes');
+				values(?, ?, ?)
 			""";
 	
-	public void insert() {
-		springJdbcTemplate.update(INSERT_QUERY);
+	private static String DELETE_QUERY = 
+			"""
+				delete from course
+				where id = ?
+			""";
+	
+	public void insert(Course course) {
+		springJdbcTemplate.update(INSERT_QUERY, 
+				course.getId(), course.getName(), course.getAuthor());
+	}
+	
+	public void deleteById(long id) {
+		springJdbcTemplate.update(DELETE_QUERY, id);
 	}
 
 }
