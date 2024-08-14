0a58cb092fcaad76bf5fcdba5422f19ee6139925
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-08-14 23:50:16 +0900

    Inserted and deleted data with SpringJDBC

---
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learn_jpa_and_hibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -0,0 +1,20 @@
+package com.in28minutes.springboot.learn_jpa_and_hibernate.course.jdbc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+	
+	@Autowired
+	private CourseJdbcRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		repository.insert();
+		
+	}
+		
+
+}
